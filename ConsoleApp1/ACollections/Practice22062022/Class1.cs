70e197c2c67d1193ceeff76acc5ece790482898e
Ajinkya Bangar <ajinkyabangar212@gmail.com>
2022-06-23 10:54:30 +0530

    commit

---
+++ ConsoleApp1/ACollections/Practice22062022/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.ACollections.Practice22062022
+{
+    class Class1
+    {
+    }
+}
---
+++ ConsoleApp1/ACollections/Practice22062022/GenricArrayList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.ACollections.Practice22062022
+{
+
+
+    
+
+    class GenricArrayList
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
