52e41face310962eed2781e1547d3b041cf98a9c
Ajinkya Bangar <ajinkyabangar212@gmail.com>
2022-06-24 05:58:52 +0530

    commit

---
+++ ConsoleApp1/ACollections/Date23062022/NonGenricArrayListConcatination1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace ConsoleApp1.ACollections.Date23062022
+{
+    class NonGenricArrayListConcatination1
+    {
+        static void Main(string[] args)
+        {
+
+            ArrayList L1 = new ArrayList() { 1 , "Aji" , 35 , 90 , 50};
+
+            ArrayList L2 = new ArrayList() {2 , "Ba" , 62 , 68 , 8 , 9 };
+
+
+            L1.AddRange(L2);
+
+            foreach(object x in L1)
+            {
+                Console.Write("   " +  x);
+            }
+
+
+
+        }
+    }
+}
