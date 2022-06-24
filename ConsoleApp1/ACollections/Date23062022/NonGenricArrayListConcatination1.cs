b72ec7724619c1b33953eb4c31e86170191d7fcb
Ajinkya Bangar <ajinkyabangar212@gmail.com>
2022-06-24 09:05:40 +0530

    Commit

--- ConsoleApp1/ACollections/Date23062022/NonGenricArrayListConcatination1.cs
+++ ConsoleApp1/ACollections/Date23062022/NonGenricArrayListConcatination1.cs
@@ -15,9 +15,21 @@ namespace ConsoleApp1.ACollections.Date23062022
             ArrayList L2 = new ArrayList() {2 , "Ba" , 62 , 68 , 8 , 9 };
 
 
-            L1.AddRange(L2);
+            foreach (object x in L1)        // list Item in L1
+            {
+                Console.Write(" " + x);
+            }
+            Console.WriteLine();
+            foreach (object x in L2)            // list Item in L2
+            {
+                Console.Write(" " + x);
+            }
 
-            foreach(object x in L1)
+            Console.WriteLine();
+
+            L1.AddRange(L2);   // Concate two List 
+
+            foreach(object x in L1)   // printing Concatination
             {
                 Console.Write("   " +  x);
             }
