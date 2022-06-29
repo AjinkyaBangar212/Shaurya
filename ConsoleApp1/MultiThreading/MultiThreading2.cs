468877896bc64f96656389e3d3bceab80f9569ad
Ajinkya Bangar <ajinkyabangar212@gmail.com>
2022-06-30 04:40:55 +0530

    Commit

---
+++ ConsoleApp1/MultiThreading/MultiThreading2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.MultiThreading
+{
+    class MultiThreading2
+    {
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            //Threading 
+
+            Console.WriteLine("Ajinkya");
+
+
+
+        }
+    }
+}
