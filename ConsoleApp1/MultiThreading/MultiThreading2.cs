9817813d2f64d854f56bef6bbec75d20211168c0
Ajinkya Bangar <ajinkyabangar212@gmail.com>
2022-06-30 14:06:30 +0530

    Commit

--- ConsoleApp1/MultiThreading/MultiThreading2.cs
+++ ConsoleApp1/MultiThreading/MultiThreading2.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace ConsoleApp1.MultiThreading
 {
     class MultiThreading2
     {
+        
+        public void MM()
+        {
+            int J = 0;
+            while (J < 10)
+            {
+                Console.WriteLine($" J =  {Thread.CurrentThread.Name}");
+                J++;
+            }
 
+        }
+
+       static void MM1()
+        {
+            int J = 0;
+            while (J < 10)
+            {
+                Console.WriteLine($" K =  {Thread.CurrentThread.Name}");
+                J++;
+            }
+
+        }
 
 
 
@@ -16,7 +38,27 @@ namespace ConsoleApp1.MultiThreading
             //Threading 
 
             Console.WriteLine("Ajinkya");
+            Console.WriteLine("Ajinkya");
+            Console.WriteLine("Ajinkya");
+            Console.WriteLine("Ajinkya");
 
+
+            MultiThreading2 obj = new MultiThreading2();
+
+            Thread T1 = new Thread(obj.MM);
+            T1.Start();
+            T1.Name = "T1";
+             Thread T2 = new Thread(MM1);
+             T2.Start();
+             T2.Name = "T2";
+ 
+
+            int i = 0;
+            while (i < 30)
+            {
+                Console.WriteLine($" I = {Thread.CurrentThread.Name} ");
+                i++;
+            }
 
 
         }
