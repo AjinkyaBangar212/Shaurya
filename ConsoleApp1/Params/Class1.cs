fb389470f1f473d0e848212e9454e9541666a438
Ajinkya Bangar <ajinkyabangar212@gmail.com>
2022-07-22 08:23:06 +0530

    commit

---
+++ ConsoleApp1/Params/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Params
+{
+    class Class1
+    {
+
+
+
+        static void method(params int[]  AA)
+        {
+
+            foreach (int xx in AA)
+            {
+                Console.WriteLine(xx);
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            method(10,20,30);
+
+
+        }
+
+    }
+}
---
+++ ConsoleApp1/Params/Class2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Params
+{
+    class Class2
+    {
+        static int method(params int[] AA)
+        {
+            int x = 0;
+            foreach (int xx in AA)
+            {
+                Console.WriteLine(xx);
+          
+            }
+            return x;
+           
+        }
+        static void Main(string[] args)
+        {
+            int re = method(10,500,30);
+
+        }
+    }
+}
