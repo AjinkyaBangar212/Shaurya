3009530c068deb369a90d6d3d59ad29961396e24
Ajinkya Bangar <ajinkyabangar212@gmail.com>
2022-07-26 10:46:57 +0530

    commit

---
+++ ConsoleApp1/collection/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.collection
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+
+            string s1 = "aayush";
+
+            char[] arr = s1.ToCharArray();
+
+            Dictionary<char, int> d1 = new Dictionary<char, int>();
+
+            foreach (char ch in arr)
+            {
+
+                if (d1.ContainsKey(ch))
+                {
+                    int currval = d1[ch];
+                    d1[ch] = currval + 1;
+                    
+                }
+                else
+                {
+                    d1.Add(ch, 1);
+                }
+
+                foreach (KeyValuePair<Char, int> kv in d1)
+                {
+                    Console.WriteLine($"{kv.Key}        {kv.Value}");
+                }
+
+            }
+
+
+
+        }
+    }
+}
--- ConsoleApp1/collection/DemoDictonary2.cs
+++ ConsoleApp1/collection/DemoDictonary2.cs
@@ -13,7 +13,8 @@ namespace ConsoleApp1.collection
             dd.Add(101,"Ajinkya");
             dd.Add(102, "Rahul");
             dd.Add(103,"Manoj");
-            
+          
+
             Console.WriteLine(dd.Count + " is Count");
             Console.WriteLine(dd[102]);
             Console.WriteLine(dd.ContainsKey(108));
