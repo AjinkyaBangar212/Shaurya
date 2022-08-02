e4cb049557cb6605055da5564004da4b5ad20948
Ajinkya Bangar <ajinkyabangar212@gmail.com>
2022-08-02 08:37:48 +0530

    Commit all

---
+++ ConsoleApp1/Revision/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Revision
+{
+    class Class1
+    {
+
+        static void Main(string[] args)
+        {
+            string nm = "Ajinkya";
+            char a = 'x';
+            int x = 10;
+            float xx = 19.20f;
+            double d = 20.50d;
+            bool b = true;
+            long l = 505050;
+
+            Console.WriteLine($"{nm} {a} {x} {xx} {d} {b} {l}");
+
+
+
+            //const keyword 
+
+            const int c = 20;
+
+
+            Console.WriteLine(c);
+
+
+            //type casting
+
+            
+        }
+    }
+}
